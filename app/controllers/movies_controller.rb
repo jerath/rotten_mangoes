f3a0d4023de87fa1640d163aa1f08b15297ea9ef
Jessica Thomas <jess.rae.thomas@gmail.com>
2015-06-24 11:02:01 -0700

    search works

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -2,6 +2,19 @@ class MoviesController < ApplicationController
 
   def index
     @movies = Movie.where("title LIKE ?", "%#{params[:title]}%").where("director LIKE ?", "%#{params[:director]}%")
+
+  # <%= select_tag(:duration, options_for_select([
+    # ['Under 90 minutes', 1], 
+    # ['Between 90 and 120 minutes', 2],
+    #  ['Over 120 minutes', 3]])) %>
+    case params[:duration]
+    when 1
+      @movies = @movies.where("duration < 90")
+    when 2
+      @movies = @movies.where("duration BETWEEN 90 AND 120")
+    when 3
+      @movies = @movies.where("duration > 120")
+    end
   end
 
   def show
