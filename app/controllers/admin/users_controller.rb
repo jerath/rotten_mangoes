26c000674c15451bfc22a321cd7707e816c8c302
Jessica Thomas <jess.rae.thomas@gmail.com>
2015-06-24 14:08:40 -0700

    delete user

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -34,7 +34,9 @@ class Admin::UsersController < ApplicationController
   end
   
   def destroy
+    name = User.find(params[:id]).firstname
     User.destroy(params[:id])
+    redirect_to admin_users_path, notice: "Successfully deleted user #{name}."
   end
 
   protected
